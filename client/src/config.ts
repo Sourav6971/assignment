826c7407fe69c5973ee08cfbfba5eea79e2956ea
sourav <mailsourav014@gmail.com>
2025-06-20 23:59:42 +0530

    added database url

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,2 +1,1 @@
-// export const  database_url="https://assignment-henna-mu.vercel.app"
-export const database_url="http://localhost:3000"
+export const  database_url="https://assignment-henna-mu.vercel.app"
